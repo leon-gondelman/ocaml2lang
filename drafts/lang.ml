fc0c8f76489a86f12fbb56be7aff1c5d543721d5
Leon Gondelman <gondelman@cs.au.dk>
2021-07-30 11:48:05 +0100

    lang.ml

--- drafts/lang.ml
+++ drafts/lang.ml
@@ -3,69 +3,16 @@
  #load "threads.cma";; *)
 
 open Unix
+open Network
 
-type ('a, 'b) sumTy = InjL of 'a | InjR of 'b
- (* type 'a opt = (unit, 'a) sumTy *)
-
-type ip_address = string
-type saddr = SADDR of (ip_address * int)
-
-(* internal *)
-let to_saddr s =
-  match s with
-    ADDR_UNIX _ -> assert false
-  | ADDR_INET (ip, p) -> SADDR (string_of_inet_addr ip, p)
-
-(* internal *)
-let of_saddr s =
-  match s with
-  | SADDR (ip, p) -> ADDR_INET (inet_addr_of_string ip, p)
+(* type ('a, 'b) sumTy = InjL of 'a | InjR of 'b *)
 
 
-(* internal *)
-exception OnlyPosTimeout
+let[@builtin] makeAddress ip port = ADDR_INET (inet_addr_of_string ip, port)
 
-(* internal *)
-let makeDecimal n =
-  let f = float_of_int n in
-  if n < 0 then raise OnlyPosTimeout;
-  let rec aux q =
-    if q < 1. then q
-    else aux (q /. 10.)
-  in aux f
+let[@builtin] udp_socket () = socket PF_INET SOCK_DGRAM 0
 
-let ip_of_sockaddr s =
-  match s with
-    ADDR_UNIX _ -> assert false
-  | ADDR_INET (ip, _) -> ip
-
-let port_of_sockaddr s =
-  match s with
-    ADDR_UNIX _ -> assert false
-  | ADDR_INET (_, p) -> p
-
-
-let ip_of_address s =
-  match s with
-  | SADDR (ip, _) -> ip
-
-let port_of_address s =
-  match s with
-  | SADDR (_, p) -> p
-
-
-let unSOME o = match o with
-    None -> assert false
-  | Some x -> x
-
-(* translate only name *)
-let makeAddress [@built-in] ip port = ADDR_INET (inet_addr_of_string ip, port)
-
-(* translate only name *)
-let socket () = socket PF_INET SOCK_DGRAM 0
-
-(* translate only name *)
-let receiveFrom skt =
+let[@builtin] receiveFrom skt =
   let buffer = Bytes.create 65536 in
   try
     match recvfrom skt buffer 0 65536 [] with
@@ -78,32 +25,51 @@ let receiveFrom skt =
   | Unix_error (EWOULDBLOCK, _, _) -> None
 
 (* translate only name *)
-let sendTo skt msg sa =
+let[@builtin] sendTo skt msg sa =
   sendto skt (Bytes.of_string msg) 0 (String.length msg) [] (of_saddr sa)
 
 (* translate only name *)
-let socketBind socket addr = bind socket (of_saddr addr)
+let[@builtin] socketBind socket addr = bind socket (of_saddr addr)
+
+exception OnlyPosTimeout
+
+let makeDecimal n =
+  let f = float_of_int n in
+  if n < 0 then raise OnlyPosTimeout;
+  let rec aux q =
+    if q < 1. then q
+    else aux (q /. 10.)
+  in aux f
 
 (* translate only name *)
-let setReceiveTimeout sh n m =
+let[@builtin] setReceiveTimeout sh n m =
   let fn = float_of_int n in
   let fm = makeDecimal m in
   Unix.setsockopt_float sh SO_RCVTIMEO (fn +. fm)
 
-(* translate only name *)
-let fork e =
+let[@builtin] fork e =
   let _ = Thread.create (fun () -> e) () in ()
 
-let findFrom e0 e1 e2 =
+let[@builtin] findFrom e0 e1 e2 =
   String.index_from_opt e0 e1 e2
 
-let strlen = String.length
 
-let s2i s = int_of_string_opt s
-
-let i2s n = string_of_int n
-
-
-let substring e0 e1 e2 =
+let[@builtin] substring e0 e1 e2 =
   try String.sub e0 e1 e2
   with Invalid_argument _ -> ""
+
+
+(* (UnOp StringLength e) *)
+let[@construct: UnOp StringLength e] strlen e = String.length e
+
+(* (UnOp StringOfInt e) *)
+let[@construct: UnOp StringOfInt e] stringOfInt e = string_of_int e
+
+(* Translate to UnOp IntOfString e *)
+let[@construct: UnOp IntOfString e] intOfString e = int_of_string_opt e
+
+
+(* Notations: *)
+(* Notation i2s e := (UnOp StringOfInt e)%E (only parsing). *)
+(* Notation s2i e := (UnOp IntOfString e)%E (only parsing). *)
+(* Notation strlen e := (UnOp StringLength e)%E (only parsing). *)
---
+++ drafts/network.ml
@@ -0,0 +1,32 @@
+
+type ip_address = string
+type saddr = SADDR of (ip_address * int)
+
+
+let to_saddr s =
+  match s with
+    ADDR_UNIX _ -> assert false
+  | ADDR_INET (ip, p) -> SADDR (string_of_inet_addr ip, p)
+
+let of_saddr s =
+  match s with
+  | SADDR (ip, p) -> ADDR_INET (inet_addr_of_string ip, p)
+
+let ip_of_sockaddr s =
+  match s with
+    ADDR_UNIX _ -> assert false
+  | ADDR_INET (ip, _) -> ip
+
+let port_of_sockaddr s =
+  match s with
+    ADDR_UNIX _ -> assert false
+  | ADDR_INET (_, p) -> p
+
+
+let[@builtin] ip_of_address s =
+  match s with
+  | SADDR (ip, _) -> ip
+
+let[@builtin] port_of_address s =
+  match s with
+  | SADDR (_, p) -> p
