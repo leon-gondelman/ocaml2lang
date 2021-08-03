a3a44dae12608c58a29bce182d5bdff3c14a90be
Mário Pereira <mjp.pereira@fct.unl.pt>
2021-08-03 23:33:18 +0100

    Create necessary sub directories.

--- bin/main.ml
+++ bin/main.ml
@@ -18,6 +18,7 @@ let mk_output output fname =
   let root_fname = String.split_on_char '/' fname in
   let concat_fname acc f = Filename.concat acc f in
   match root_fname with
+  | [x] -> x
   | _ :: xs -> List.fold_left concat_fname output xs
   | _ -> assert false (* TODO *)
 
@@ -34,9 +35,25 @@ let pp_deps fmt (dep, path, _) =
   fprintf fmt "From @[%a@] Require Import %s."
     (pp_print_list ~pp_sep:pp_dot pp_print_string) path dep
 
+let perm = 0o775
+
+let create_sub_dirs dirname =
+  let dirs = String.split_on_char '/' dirname in
+  let fdir acc f = Filename.concat acc f in
+  let mkdir f =
+    if not (Sys.file_exists f) then Unix.mkdir f perm in
+  let acc = ref "" in
+  List.iter (fun f -> acc := fdir !acc f; mkdir !acc) dirs
+
+let create_sub_dirs dirname =
+  if dirname <> "." then create_sub_dirs dirname
+
 let pp_program fname prog =
-  let _output = ml_project.ml_output in
+  let output = ml_project.ml_output in
   let fout_name = (String.uncapitalize_ascii fname) ^ ".v" in
+  let fout_name = mk_output output fout_name in
+  let dirname = Filename.dirname fout_name in
+  create_sub_dirs dirname;
   let cout = open_out fout_name in
   let fout = formatter_of_out_channel cout in
   let decls = prog.prog_body in
