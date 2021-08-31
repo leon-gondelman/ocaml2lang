8c9dc1a846e935c0c2dbaf775bc6be85b0b4d1f7
Abel Nieto <abeln@cs.au.dk>
2021-08-31 12:09:41 -0400

    Add support for if-expressions without else clause

--- lib/translate_aneris.ml
+++ lib/translate_aneris.ml
@@ -351,6 +351,12 @@ let rec split_coqparams info (acc : (ident * argty option) list) expr =
   | _ ->
      begin sanity_check_params info expr; (List.rev acc, expr) end
 
+let synth_expression desc =
+  { P.pexp_desc = desc;
+    P.pexp_loc = Location.none;
+    P.pexp_loc_stack = [];
+    P.pexp_attributes = [] }
+
 let rec value_binding_notbuiltin
           ~isrec
           (info : info)
@@ -598,8 +604,11 @@ and expression info expr =
      let expr2 = expression info e2 in
      let expr3 = expression info e3 in
      If (expr1, expr2, expr3)
-  | Pexp_ifthenelse (_, _, None) ->
-     assert false
+  | Pexp_ifthenelse (e1, e2, None) ->
+     let unit_expr = synth_expression (
+       Pexp_construct ({txt = Lident "()"; loc = Location.none}, None)) in
+     let expr' = {expr with P.pexp_desc = Pexp_ifthenelse (e1, e2, Some unit_expr)} in
+     expression info expr'
   | Pexp_let (Nonrecursive, [val_bind], e2) ->
       let (id, _, expr) =
           value_binding_notbuiltin ~isrec:false info val_bind in
